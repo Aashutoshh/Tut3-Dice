3cbcd82d30883d39f9ea24fb9f304a08b321058d
Aashutoshh <214506607@stu.ukzn.ac.za>
2016-03-06 10:23:57 +0200

    Add global average functions

--- Dice.cpp
+++ Dice.cpp
@@ -1,6 +1,26 @@
 #include "Dice.h"
 #include "cstdlib" 
 
+float average(Dice obj,  int noOfRolls){
+	float sum= 0.0;
+	for ( int i = 0; i < noOfRolls; i++)
+	{
+		sum += obj.roll();
+	}
+
+	return sum / noOfRolls;
+}
+
+
+float average( int arr[], int noOfInt){
+	float sum = 0.0;
+	for (int i = 0; i < noOfInt; i++)
+	{
+		sum += arr[i];
+	}
+	return sum / noOfInt;
+}
+
 class Dice
 {
 private:
